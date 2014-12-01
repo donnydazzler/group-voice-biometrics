52ab1e760cb36b8bf78d34efdb16ca27bcd4c3cb
Greg Millward <greg_millward@cable.comcast.com>
2014-12-01 17:13:22 -0500

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('groupVoiceBiometricsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
