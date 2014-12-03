634f50572c2abfa1f3f52f6fdb83ed3836a672da
Greg Millward <greg_millward@cable.comcast.com>
2014-12-03 08:51:19 -0500

    Removed thing server-side endpoint

--- server/routes.js
+++ server/routes.js
@@ -11,8 +11,7 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/authentications', require('./api/authentication'));
   app.use('/api/enrollments', require('./api/enrollment'));
-  app.use('/api/things', require('./api/thing'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
