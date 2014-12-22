9cb1955d8353817d54d7b2dc79e2b62035fbb477
Greg Millward <greg_millward@cable.comcast.com>
2014-12-22 17:35:03 -0500

    Cleaned up config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  groupId: 1,
+  VOICEIT_DEV_ID: process.env.VOICEIT_DEV_ID,
   email: process.env.VOICEIT_EMAIL,
   password: process.env.VOICEIT_PWD,
-  VOICEIT_DEV_ID: process.env.VOICEIT_DEV_ID,
   accuracy: 0, // recommended 0-2, but can be 3-5
   confidence: 90, // recommended 90-92, but can be 88-89
   accuracyPasses: 5,
   accuracyPassIncrement: 2,
-  voiceprintPhrase: 'Never forget tomorrow is a new day',
   voiceprintTextConfidenceThreshold: 0.5
 };
